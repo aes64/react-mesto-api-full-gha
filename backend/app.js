773eae8c95b0687ccf1ed1aada0b1838c02884e8
Elena Andreeva <aes1999@yandex.ru>
2023-06-08 23:40:44 +0500

    cors 8

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,12 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   res.header('Access-Control-Allow-Origin', "*");
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; 
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  } 
   next();
 });
 
