9fc96860177f0c47eed33683183b1de59e2ff1ba
Elena Andreeva <aes1999@yandex.ru>
2023-06-08 23:09:36 +0500

    cors 2

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "*");
   }
   next();
 });
